239fb659733abb2f99679754d584cd1e6b441bad
khanh-it <phikhanh.8x@gmail.com>
2018-05-26 00:29:16 +0700

    o Install 'redux'...(WIP)

---
+++ App.ReactRedux.js
@@ -0,0 +1,14 @@
+// In App.js in a new project
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// use styles as usual
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={[]}>
+        <Text style={[]}>Hello</Text>
+      </View>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { AppRegistry } from 'react-native';
 // import App from './App';
 // import App from './App.ReactNavigation';
 // import App from './App.ReactNativeEmojis';
-import App from './App.ReactNativeExtendedStyleSheet';
+// import App from './App.ReactNativeExtendedStyleSheet';
+import App from './App.ReactRedux';
 
 AppRegistry.registerComponent('rn_airtrip', () => App);
