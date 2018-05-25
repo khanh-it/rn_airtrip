f20371d335b224f2ac6a4a32a936b50708766c80
khanh-it <phikhanh.8x@gmail.com>
2018-05-25 23:33:37 +0700

    o Install 'react-native-extended-stylesheet'.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { AppRegistry } from 'react-native';
 // import App from './App';
 // import App from './App.ReactNavigation';
-import App from './App.ReactNativeEmojis';
+// import App from './App.ReactNativeEmojis';
+import App from './App.ReactNativeExtendedStyleSheet';
 
 AppRegistry.registerComponent('rn_airtrip', () => App);
