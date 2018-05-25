513c8232b6f828c2a997b4ee1e16e4f3b50dbb53
khanh-it <phikhanh.8x@gmail.com>
2018-05-25 23:11:37 +0700

    o Install 'react-navigation'.

---
+++ App.ReactNavigation.js
@@ -0,0 +1,27 @@
+// In App.js in a new project
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+});
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './App';
+import App from './App.ReactNavigation';
 
 AppRegistry.registerComponent('rn_airtrip', () => App);
