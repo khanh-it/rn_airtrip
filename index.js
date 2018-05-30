20ae0daaae7659c2ad80b09cb461ab25e2eee736
KhanhDTP <khanhdtp@evolableasia.vn>
2018-05-30 15:07:22 +0700

    o Implement vector icons.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import prjGlob from './app/global'; // project's globals
 // import App from './App.ReactNativeEmojis';
 // import App from './App.ReactNativeExtendedStyleSheet';
 // import App from './App.ReactRedux';
-import App from './App.SplashScreen';
+// import App from './App.SplashScreen';
+import App from './App.ReactNativeVectorIcons';
 
 AppRegistry.registerComponent('rn_airtrip', () => App);
