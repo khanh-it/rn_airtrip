6644fd4cc1aabc4f15f9e6c2c7d464fdc5ccf792
KhanhDTP <khanhdtp@evolableasia.vn>
2019-01-10 15:26:59 +0700

    Update RN to 0.57.

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,7 +1,9 @@
 // https://gist.github.com/parshap/e3063d9bf6058041b34b26b7166fd6bd
 module.exports = {
+  resolver: {
     extraNodeModules: {
       'react-native-my': require.resolve('./app/polyfills/react-native-my'),
       crypto: require.resolve('./app/polyfills/crypto'),
     },
+  }
 };
